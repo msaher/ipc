8bb0367b6a7c5eb58c16ed30496cdae61f19628a
saher <msaher.shair@gmail.com>
2023-04-20 23:58:44 +0300

    feat: add CidrParseError

--- src/ipv4_cidr/error.rs
+++ src/ipv4_cidr/error.rs
@@ -11,3 +11,35 @@ impl fmt::Display for InvalidPrefixErr {
     }
 }
 
+#[derive(Debug, Clone)]
+pub enum CidrParseError {
+    TokenErr,
+    AddrErr(AddrParseError),
+    PrefixErr,
+    InvalidErr(InvalidPrefixErr),
+}
+
+impl fmt::Display for CidrParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::AddrErr(add_err) => add_err.fmt(f),
+            Self::TokenErr => write!(f, "must have one / character"),
+            Self::PrefixErr => write!(f, "cant find a valid prefix"),
+            Self::InvalidErr(e) => e.fmt(f)
+        }
+    }
+
+}
+
+impl From<AddrParseError> for CidrParseError {
+    fn from(e: AddrParseError) -> CidrParseError {
+        return CidrParseError::AddrErr(e);
+    }
+}
+
+impl From<ParseIntError> for CidrParseError {
+    fn from(_e: ParseIntError) -> CidrParseError {
+        return CidrParseError::PrefixErr;
+    }
+}
+
